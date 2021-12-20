04dca053b9c6f8d9288a44d58a0081e3acefb5bc
Gary Zhang <gzhang@primeobjects.com>
2021-12-19 22:17:51 -0800

    parameter check

--- libs/twilio-sync-document.ts
+++ libs/twilio-sync-document.ts
@@ -3,12 +3,14 @@
 //  This source code is licensed under the MIT license.
 //  The detail information can be found in the LICENSE file in the root directory of this source tree.
 
-
+import { isNonEmptyString } from 'douhub-helper-util';
 import {assign, isInteger} from 'lodash';
 import {initClient} from './twilio-sync-token';
 
 export const deleteSyncDocument = async (id:string): Promise<Record<string,any>> => {
     const twilioService = await initClient();
+    if (!isNonEmptyString(id)) throw 'The id is required.';
+
     return new Promise((resolve, reject) => {
         twilioService
             .documents(id)
@@ -25,6 +27,9 @@ export const deleteSyncDocument = async (id:string): Promise<Record<string,any>>
 export const createSyncDocument = async (data:Record<string,any>): Promise<Record<string,any>> => {
 
     const twilioService = await initClient();
+    
+    if (!isNonEmptyString(data.id)) throw 'The data.id is required.';
+
     const document = assign({ data }, { uniqueName: data.id }, isInteger(data.ttl) && data.ttl > 0 ? { ttl: data.ttl } : {});
     
     return new Promise((resolve, reject) => {
@@ -43,7 +48,9 @@ export const createSyncDocument = async (data:Record<string,any>): Promise<Recor
 export const retrieveSyncDocument = async (id: string) : Promise<Record<string,any>> => {
 
     const twilioService = await initClient();
-    
+
+    if (!isNonEmptyString(id)) throw 'The id is required.';
+
     return new Promise((resolve, reject) => {
         twilioService
             .documents(id)
@@ -60,6 +67,9 @@ export const retrieveSyncDocument = async (id: string) : Promise<Record<string,a
 export const updateSyncDocument = async (data:Record<string,any>): Promise<Record<string,any>> => {
 
     const twilioService = await initClient();
+   
+    if (!isNonEmptyString(data.id)) throw 'The data.id is required.';
+
     const document = assign({ data }, { uniqueName: data.id }, isInteger(data.ttl) && data.ttl > 0 ? { ttl: data.ttl } : {});
    
     return new Promise((resolve, reject) => {
@@ -77,10 +87,14 @@ export const updateSyncDocument = async (data:Record<string,any>): Promise<Recor
 
 export const upsertSyncDocument = async (data:Record<string,any>): Promise<Record<string,any>> => {
 
-    if (await retrieveSyncDocument(data.id)) {
+    try
+    {
+        await retrieveSyncDocument(data.id);
         return await updateSyncDocument(data);
     }
-    else {
+    catch(error)
+    {
+        console.error(error);
         return await createSyncDocument(data);
     }
 };
--- libs/twilio-sync-list.ts
+++ libs/twilio-sync-list.ts
@@ -3,201 +3,215 @@
 //  This source code is licensed under the MIT license.
 //  The detail information can be found in the LICENSE file in the root directory of this source tree.
 
-import {assign, isInteger} from 'lodash';
-import {initClient} from './twilio-sync-token';
+import { isNonEmptyString } from 'douhub-helper-util';
+import { assign, isInteger } from 'lodash';
+import { initClient } from './twilio-sync-token';
 
-
-export const retrieveSyncList = async (id:string): Promise<Record<string,any>> => {
+export const retrieveSyncList = async (id: string): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
-    
+
+    if (!isNonEmptyString(id)) throw 'The id is required.';
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(id)
             .fetch()
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const deleteSyncList = async (id:string): Promise<Record<string,any>> => {
+export const deleteSyncList = async (id: string): Promise<Record<string, any>> => {
     const twilioService = await initClient();
-    
+
+    if (!isNonEmptyString(id)) throw 'The id is required.';
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(id)
             .remove()
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const createSyncList = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const createSyncList = async (data: Record<string, any>): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
-    
+
+    if (!isNonEmptyString(data.id)) throw 'The data.id is required.';
+
     const list = assign({ data }, { uniqueName: data.id }, isInteger(data.ttl) && data.ttl > 0 ? { ttl: data.ttl } : {});
-    
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists
             .create(list)
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
 
-export const updateSyncList = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const updateSyncList = async (data: Record<string, any>): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
+
+    if (!isNonEmptyString(data.id)) throw 'The data.id is required.';
     
     const list = assign({ data }, { uniqueName: data.id }, isInteger(data.ttl) && data.ttl > 0 ? { ttl: data.ttl } : {});
-   
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(data.id)
             .update(list)
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const upsertSyncList = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const upsertSyncList = async (data: Record<string, any>): Promise<Record<string, any>> => {
 
-    if (await retrieveSyncList(data.id)) {
+    try {
+        await retrieveSyncList(data.id);
         return await updateSyncList(data);
     }
-    else {
+    catch (error) {
+        console.error(error);
         return await createSyncList(data);
     }
 };
 
 //Start List Item
 
-export const retrieveSyncListItem = async (listId:string , index: number): Promise<Record<string,any>> => {
+export const retrieveSyncListItem = async (listId: string, index: number): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
+
+    if (!isNonEmptyString(listId)) throw 'data.listId is required.';
+    if (!isInteger(index)) throw 'data.index is required.';
+
 
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(listId)
             .syncListItems(index)
             .fetch()
-            .then((item:Record<string,any>) => {
+            .then((item: Record<string, any>) => {
                 resolve(item);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const createSyncListItem = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const createSyncListItem = async (data: Record<string, any>): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
-    
-
     const listId = data.listId;
-    if (!listId) throw 'data.listId is required.';
+    if (!isNonEmptyString(listId)) throw 'data.listId is required.';
 
     const item = assign({ data }, isInteger(data.ttl) && data.ttl > 0 ? { ttl: data.ttl } : {});
-   
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(listId)
             .syncListItems
             .create(item)
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const updateSyncListItem = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const updateSyncListItem = async (data: Record<string, any>): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
-    
+
     const listId = data.listId;
-    if (!listId) throw 'data.listId is required.';
+    if (!isNonEmptyString(listId)) throw 'data.listId is required.';
 
     const index = data.index;
-    if (!index) throw 'data.index is required.';
+    if (!isInteger(index)) throw 'data.index is required.';
 
     const item = assign({ data }, isInteger(data.ttl) && data.ttl > 0 ? { ttl: data.ttl } : {});
- 
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(listId)
             .syncListItems(index)
             .update(item)
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const deleteSyncListItem = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const deleteSyncListItem = async (listId: string, index: number): Promise<Record<string, any>> => {
 
     const twilioService = await initClient();
 
-    const listId = data.listId;
-    if (!listId) throw 'data.listId is required.';
+    if (!isNonEmptyString(listId)) throw 'The listId is required.';
+    if (!isInteger(index)) throw 'The index is required.';
 
-    const index = data.index;
-    if (!index) throw 'data.index is required.';
-   
-   
+
     return new Promise((resolve, reject) => {
         twilioService
             .syncLists(listId)
             .syncListItems(index)
             .remove()
-            .then((list:Record<string,any>) => {
+            .then((list: Record<string, any>) => {
                 resolve(list);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 reject(error);
             });
     });
 };
 
-export const upsertSyncListItem = async (data:Record<string,any>): Promise<Record<string,any>> => {
+export const upsertSyncListItem = async (data: Record<string, any>): Promise<Record<string, any>> => {
 
     const listId = data.listId;
-    if (!listId) throw 'data.listId is required.';
+    if (!listId) throw 'The data.listId is required.';
 
     const index = data.index;
-    if (!index) throw 'data.index is required.';
-   
 
-    if (await retrieveSyncListItem(listId, index)) {
-        return await updateSyncListItem(data);
+    try {
+        if (isInteger(index)) {
+            await retrieveSyncListItem(listId, index);
+            return await updateSyncListItem(data);
+        }
+        else {
+            return await createSyncListItem(data);
+        }
     }
-    else {
-        return await createSyncList(data);
+    catch (error) {
+        console.error(error);
+        return await createSyncListItem(data);
     }
 };
 
